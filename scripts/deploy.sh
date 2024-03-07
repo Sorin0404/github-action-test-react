7372d33d8401b65052768958bda47b2847699798
Sorin0404 <haewong27@gmail.com>
2024-03-07 22:00:34 +0900

    code deploy test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,10 @@
 cd /home/ubuntu/build/temp
 
 # 압축 해제
-unzip out.zip -d /home/ubuntu/build/temp
+unzip build.zip -d /home/ubuntu/build/temp
 
-# mv /home/ubuntu/build/temp/out/* /home/ubuntu/build/
-cp -r /home/ubuntu/build/temp/out/* /home/ubuntu/build/
+# mv /home/ubuntu/build/temp/build/* /home/ubuntu/build/
+cp -r /home/ubuntu/build/temp/build/* /home/ubuntu/build/
 
 # 임시 파일 정리
 rm -rf /home/ubuntu/build/temp
